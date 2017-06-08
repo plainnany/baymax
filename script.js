a439ec3c5a84dea98c3d838a8820d27ceaba1463
plainnany <plainnany@gmail.com>
2017-06-08 21:29:46 +0800

    一些信息

---
+++ script.js
@@ -0,0 +1,89 @@
+var wrap = document.createElement('div');
+wrap.className='wrap';
+document.body.appendChild(wrap);
+
+var head, body, chest, belly, foot;
+ 
+var arr=[
+  function(){  
+     head = document.createElement('div');
+     head.className='head';
+     wrap.appendChild(head);
+    
+  },
+  function(){
+    
+    var eye1=document.createElement('div');
+    eye1.className='eye1';
+    head.appendChild(eye1);
+  },
+  function(){
+    var eye2=document.createElement('div');
+    eye2.className='eye2';
+    head.appendChild(eye2);
+  },
+  function(){
+    
+    var line=document.createElement('div');
+    line.className='line';
+    head.appendChild(line);
+    
+  },
+  
+  function(){
+    body=document.createElement('div');
+    body.className='body';
+    wrap.appendChild(body);
+    chest=document.createElement('div');
+    chest.className='chest';
+    body.appendChild(chest);
+  },
+  function(){
+    var heart=document.createElement('div');
+    heart.className='heart';
+    chest.appendChild(heart);
+  },
+  function(){
+    belly=document.createElement('div');
+    belly.className='belly';
+    body.appendChild(belly);
+    var cover=document.createElement('div');
+    cover.className='cover';
+    belly.appendChild(cover);
+  },
+  function(){
+    var leftArm=document.createElement('div');
+    leftArm.className='left-arm';
+    body.appendChild(leftArm)
+  },
+  function(){
+    var rightArm=document.createElement('div');
+    rightArm.className='right-arm';
+    body.appendChild(rightArm)
+  },
+  function(){
+    foot=document.createElement('div');
+    foot.className='foot';
+    wrap.appendChild(foot);
+   
+    var leftFoot=document.createElement('div');
+    leftFoot.className='left-foot';
+    foot.appendChild(leftFoot)
+  },
+  function(){
+    var rightFoot=document.createElement('div');
+    rightFoot.className='right-foot';
+    foot.appendChild(rightFoot);
+  }
+   
+];
+
+document.onclick=function(){
+
+  var fn=arr.shift();
+  fn && fn.call(); 
+  if(fn===undefined){
+    alert("大白已经画好啦~")
+  }
+}
+
